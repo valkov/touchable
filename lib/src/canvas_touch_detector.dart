9d9a1e9ef993b2cad4dd5e7d45a4aa1d0f619ff3
Valentin Kovalski <valentin.kovalskyi@valtech.com>
2021-09-16 15:58:07 +0200

    disabled pan gesture detection on canvas

--- lib/src/canvas_touch_detector.dart
+++ lib/src/canvas_touch_detector.dart
@@ -86,21 +86,21 @@ class _CanvasTouchDetectorState extends State<CanvasTouchDetector> {
           onForcePressUpdate: (tapDetail) {
             touchController.add(Gesture(GestureType.onForcePressUpdate, tapDetail));
           },
-          onPanStart: (tapDetail) {
-            touchController.add(Gesture(GestureType.onPanStart, tapDetail));
-          },
-          onPanUpdate: (tapDetail) {
-            touchController.add(Gesture(GestureType.onPanUpdate, tapDetail));
-          },
-          onPanDown: (tapDetail) {
-            touchController.add(Gesture(GestureType.onPanDown, tapDetail));
-          },
-          onSecondaryTapDown: (tapDetail) {
-            touchController.add(Gesture(GestureType.onSecondaryTapDown, tapDetail));
-          },
-          onSecondaryTapUp: (tapDetail) {
-            touchController.add(Gesture(GestureType.onSecondaryTapUp, tapDetail));
-          },
+          // onPanStart: (tapDetail) {
+          //   touchController.add(Gesture(GestureType.onPanStart, tapDetail));
+          // },
+          // onPanUpdate: (tapDetail) {
+          //   touchController.add(Gesture(GestureType.onPanUpdate, tapDetail));
+          // },
+          // onPanDown: (tapDetail) {
+          //   touchController.add(Gesture(GestureType.onPanDown, tapDetail));
+          // },
+          // onSecondaryTapDown: (tapDetail) {
+          //   touchController.add(Gesture(GestureType.onSecondaryTapDown, tapDetail));
+          // },
+          // onSecondaryTapUp: (tapDetail) {
+          //   touchController.add(Gesture(GestureType.onSecondaryTapUp, tapDetail));
+          // },
         ));
   }
 
